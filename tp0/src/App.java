59f302133417a0b96a7e70bac89516fe022e2ebe
jennaguilar <ajenniferlucia@gmail.com>
2025-09-27 01:17:20 -0300

    Encacapsulamiento y uso de set&get en app.java y Estudiantes.java

--- tp0/src/App.java
+++ tp0/src/App.java
@@ -4,15 +4,15 @@
 public class App { /*App java es como si fuera nuestro main, que nos permite crear todo lo que nosotros ibamos a trabajar dentro del main*/
     public static void main(String[] args) throws Exception {
     
-        /*1-crear un objeto de la clase estudiante*/   //new Estudiante constructor vacio para crear un objeto sin valores iniciales
-        Estudiante e1 = new Estudiante();  //new creando un objeto de la clase    e1→ nombre de la variable cualquiera
-        e1.nombre = "Kevin";
-        e1.apellido = "Lucero";
-        e1.edad = 22;
-        e1.carrera = "Ingenieria en sistemas";
-        e1.promedio = 9.5f;
+        //1- Crear estudiante usando conestructor vacio + los setters
+        Estudiante e1 = new Estudiante();  // objeto en blanco
+        e1.setNombre("Kevin");      //asigno nombre set con valor
+        e1.setApellido("Lucero"); //asigno apellido set con valor
+        e1.setEdad(22);      //asigno edad set con valor
+        e1.setCarrera("Ingenieria en sistemas"); //asigno carrera set con valor
+        e1.setPromedio(9.5f); //asigno promedio set con valor
 
-        /*2-crear un objeto de la clase estudiante usando el constructor con parametros*/
+        //2-crear un objeto de la clase estudiante usando el constructor con parametros*/
         Estudiante e2 = new Estudiante("Selena", "Gomez", 21, "Arquitectura", 9.0f);
         Estudiante e3 = new Estudiante("Lautaro", "Martinez", 24, "Derecho", 8.5f);
         
@@ -20,16 +20,12 @@ public class App { /*App java es como si fuera nuestro main, que nos permite cre
         Estudiante[] estudiantes = {e1, e2, e3};
       
         //4- Recorrer el arreglo e imprimir los datos de cada estudiante
+        // se utiliza el get para obtener los valores de los atributos
         for (Estudiante est : estudiantes) { //forch-each recorre cada elemento del arreglo
-            System.out.println("Estudiante: " + est.nombre + " " + est.apellido + " | Promedio: " + est.promedio);
+            System.out.println("Estudiante: " + est.getNombre()+ " " + est.getApellido() + " | Promedio: " + est.getPromedio());
         } //est es el objeto temporal que representa cada estudiante en bucle
       
       
-      
-      
-      
-        //Estudiante maria = new("jorge", "lucero", "23","abogado", "8")*/
-        
         //Materia algebra = new Materia();
     }
 }
--- tp0/src/Estudiante.java
+++ tp0/src/Estudiante.java
@@ -1,25 +1,86 @@
 // Estudiante.java
 
 public class Estudiante {
-    //1) atributos de la clase estudiante
-    String nombre;
-    String apellido;
-    int edad;
-    String carrera;
-    float promedio;
+    //1) atributos privados encapsulados
+    // con encapsulamiento no se puede acceder directamente a los atributos
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String carrera;
+    private float promedio; 
 
-public Estudiante(){
-
-}
-
-
+    //2) constructor vacio
+    public Estudiante(){
+    }
+    // constructor con parametros
     public Estudiante (String nombre, String apellido, int edad, String carrera, float promedio){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.carrera = carrera;
-        this.promedio = promedio;
+        // Usamos Setters para asignar valores a los atributos
+        this.setNombre(nombre);        
+        this.setApellido(apellido);
+        this.setEdad(edad);
+        this.setCarrera(carrera);
+        this.setPromedio(promedio);
+    }
+
+    //3) Metodos get y set
+    // Setter → sirve para "leer" el valor de un atributo
+    // Getter → sirve para "asignar o cambiar" un valor a un atributo
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        // Validar que el nombre no sea nulo o vacío
+        if (nombre == null || nombre.isEmpty()) {
+            System.out.println(("El nombre no puede estar vacio"));
+        }else{
+        this.nombre = nombre; // this.nombre atributo de la clase
+    }
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+    if (apellido == null || apellido.isEmpty()) {
+        System.out.println(("El apellido no puede estar vacio"));
+    } else{
+    this.apellido = apellido;
     }
 }
 
+    public int getEdad() {
+        return edad;
+    }
 
+    public void setEdad(int edad) {
+        // Validar que la edad debe ser mayor a 16
+        if(edad < 16){
+            System.out.println("La edad debe ser mayor a 16");
+        } else{
+            this.edad = edad;
+        }
+    }
+ 
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        // validar promedio entre 0 y 10
+        if(promedio < 0 || promedio > 10){
+            System.out.println("El promedio debe estar entre 0 y 10");
+        } else{
+            this.promedio = promedio;
+        }
+    }
+}
